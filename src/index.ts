6c762c277665b538c1d048654c409aedaa7e6d6d
Eduardo Nunes Pereira <eduardonunesp@gmail.com>
2018-06-06 19:16:49 -0300

    Better handle for multiple accounts
    * Version 0.1.1
    * Added createExtraAccounts function which is useful while testing in
    order to create many accounts as needed to be used on truffle test
    * Added getProviderEngine getter to return the LoomProvider allowing to
    LoomProvider functions directly if needed
    * Documentation for constructor and functions
    * Clearing connection after 3 seconds of non activity

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,73 @@
-import {
-  CryptoUtils,
-  Client,
-  LoomProvider,
-  LocalAddress,
-  NonceTxMiddleware,
-  SignedTxMiddleware
-} from 'loom-js'
+import { CryptoUtils, Client, LoomProvider } from 'loom-js'
 
 class TruffleLoomProvider {
   private _engine: LoomProvider
   private _timerHandler: any
 
+  /**
+   * Constructs a new TruffleLoomProvider using LoomProvider to provide connection to Loom DAppChain.
+   * @param chainId DAppChain identifier.
+   * @param writeUrl Host & port to send txs, specified as "<protocol>://<host>:<port>".
+   * @param readUrl Host & port of the DAppChain read/query interface, this should only be provided
+   *                if it's not the same as `writeUrl`.
+   * @param privateKey Account private key in Base64 string format
+   */
   constructor(chainId: string, writeUrl: string, readUrl: string, privateKey: string) {
     const _privateKey = CryptoUtils.B64ToUint8Array(privateKey)
-    const publicKey = CryptoUtils.publicKeyFromPrivateKey(_privateKey)
     const client = new Client(chainId, writeUrl, readUrl)
 
-    client.txMiddleware = [
-      new NonceTxMiddleware(publicKey, client),
-      new SignedTxMiddleware(_privateKey)
-    ]
-
-    this._engine = new LoomProvider(client)
-    this._engine.addAccounts(LocalAddress.fromPublicKey(publicKey).toString())
+    this._engine = new LoomProvider(client, _privateKey)
     this._engine.on('error', (err: any) => {
       console.error('Error detected within Truffle process:', err)
     })
   }
 
+  /**
+   * Returns provider engine (LoomProvider)
+   *
+   * @returns Returns the LoomProvider
+   */
+  getProviderEngine(): LoomProvider {
+    return this._engine
+  }
+
+  /**
+   * Create extra accounts
+   *
+   * Useful for create new accounts to running tests on Truffle
+   *
+   * @param quantity How many accounts to be created
+   */
+  createExtraAccounts(quantity: number) {
+    const privateKeys = Array.from(Array(quantity).keys()).map(() =>
+      CryptoUtils.generatePrivateKey()
+    )
+    this._engine.addAccounts(privateKeys)
+  }
+
+  /**
+   * Send async calls to LoomProvider
+   *
+   * @param payload RPC call from Truffle
+   * @param callback Callback function (err, value) => ()
+   */
   sendAsync(payload: any, callback: Function) {
     // Required to kill connection and not hang the process
-    if (payload.method === 'eth_uninstallFilter') {
-      if (this._timerHandler) {
-        clearTimeout(this._timerHandler)
-      }
-
-      // Five seconds after uninstall filters
-      this._timerHandler = setTimeout(() => {
-        this._engine.disconnect()
-      }, 5000)
+    if (this._timerHandler) {
+      clearTimeout(this._timerHandler)
     }
+
+    // Five seconds after uninstall filters
+    this._timerHandler = setTimeout(() => {
+      this._engine.disconnect()
+    }, 3000)
 
     this._engine.sendAsync(payload, callback)
   }
 
+  /**
+   * Same from sendAsync
+   */
   send(payload: any, callback: Function) {
     return this._engine.send(payload, callback)
   }
